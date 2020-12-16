1c2f16a2255bc90ef3a1e9ae60d9644961cef34c
Sylvio Sell <info@maitag.de>
2020-12-16 21:02:57 +0100

    fixin peote-view sample

--- samples/peote-view/src/ManualConstraints.hx
+++ samples/peote-view/src/ManualConstraints.hx
@@ -14,7 +14,7 @@ import peote.layout.Size;
 import layouted.LayoutedSprite;
 import layouted.LayoutedDisplay;
 
-class Main extends lime.app.Application
+class ManualConstraints extends lime.app.Application
 {
 	var peoteView:PeoteView;
 	var display:LayoutedDisplay;
@@ -50,29 +50,23 @@ class Main extends lime.app.Application
 				
 		
 		// init a layout
-		var displayLC = new LayoutContainer(ContainerType.BOX, display,
-		[ 
-			new VBox( green,
-			{
-				left: Size.min(100), // can be scale high but not lower as min-value
-				width:Size.limit(300, 400), // can be scale from min to max-value
-				right:Size.max(200), // can be scale from 0 to max-value
-				// right:10 // or can be a fixed value.. same as .limit(10,10)
+		var redBox = new Box(red);
+		var blueBox = new Box(blue);
+		var yellowBox = new Box(yellow);
+		redBox.layout = {left:100};
+		
+		var greenBox = new HBox(green,
+			[	redBox,
+				blueBox,
+				yellowBox
+			]
+		);
 				
-				// for "span" they are reaching its min and max at the same time while scaling
-				// in a row, but can be scaled higher as max
-				top:   Size.span(50, 100),
-				height:Size.span(200, 400),
-				bottom:Size.span(50, 100),
-			},
-			// childs
-			[
-				// Box is shortcut for LayoutContainer(ContainerType.BOX, ...)
-				new Box( red,   {left:0, width:300, height:100, bottom:Size.min(100)} ),
-				new Box( blue,  {right:0, width:300, height:100, bottom:0} ),
-				new Box( yellow,{width:100, height:Size.limit(100, 300)} )
-			])
-		]);
+		var displayLC = new Box(display,
+			[ 
+				greenBox
+			]
+		);
 		
 		displayLC.init();
 		
@@ -80,16 +74,17 @@ class Main extends lime.app.Application
 		
 		rootLayoutContainer = displayLC;
 		
+		// custom constraints: siehe Kommentare in LayoutContainer -> addConstraintsHBOX 
+		//displayLC.addConstraints([
+			//(redBox._width == blueBox._width-50) | Strength.create(0, 900, 0),
+			//(yellowBox._width == greenBox._width / 2) | Strength.create(0, 900, 0),
+		//]);		
+		//greenBox.removeConstraints(); // no param -> remove all
 		
-		// TODO: show/hide all layoutElements
-		// greenLC.hide();
-		// greenLC.show();
+		//var redConstraint:Constraint = (redBox._width == redBox._height) | Strength.create(0, 0, 900);
+		//redBox.addConstraint(redConstraint);
+		//redBox.removeConstraint(redConstraint);
 		
-		// TODO: changing layout dynamically
-		// greenLC.layout.height = 100;
-		// greenLC.layout.bottom = 200;
-		
-		// TODO: adding removing childs dynamically
 	
 
 	}
