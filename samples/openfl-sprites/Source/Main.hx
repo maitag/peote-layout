d0d16c47883807fae92dd90f4fd68ae5c4b6ea16
Sylvio Sell <info@maitag.de>
2020-12-17 19:23:16 +0100

    codecleaning for openfl-sample

--- samples/openfl-sprites/Source/Main.hx
+++ samples/openfl-sprites/Source/Main.hx
@@ -11,92 +11,90 @@ import peote.layout.Size;
 
 class Main extends Sprite {
 	
-	var rootLayoutContainer:LayoutContainer;
-	var display:Sprite;
+	var layoutContainer:LayoutContainer;
 	
 	public function new () {
 		
 		super ();
-
-		// background
-		display = new Sprite();
-		display.graphics.beginFill(0x333333);
-		display.graphics.drawRect(0, 0, Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
-		display.graphics.endFill();
-		Lib.current.stage.addChild(display);
-
 		
 		// add some graphic elements
-		var green = new LayoutedSprite(0x00ff00);
-		var red = new LayoutedSprite(0xff0000);
-		var blue = new LayoutedSprite(0x0000ff);
+		var gray   = new LayoutedSprite(0x555555);
+		var green  = new LayoutedSprite(0x00ff00);
 		var yellow = new LayoutedSprite(0xffff00);
-				
+		var red    = new LayoutedSprite(0xff0000); red.alpha  = 0.7;
+		var blue   = new LayoutedSprite(0x0000ff); blue.alpha = 0.7;
 		
-		// init a layout
-		var greenLC = new LayoutContainer(ContainerType.BOX, green,
+		
+		// init layout and bind to graphic elements
+		layoutContainer = new Box( gray, // without layout param it fully scales
+		[	
+			// only one child container inside grey box
+			new Box( green,
 			{
-				left: Size.min(100), // can be scale high but not lower as min-value
-				width:Size.limit(300, 400), // can be scale from min to max-value
-				right:Size.max(200), // can be scale from 0 to max-value
-				// right:10 // or can be a fixed value.. same as .limit(10,10)
+				// layout for size (width/height) and spacing (left, right, top, bottom) to outer container
+				left: Size.max(100),        // "max"   scales from 0 to max-value
+				width:Size.limit(300, 800), // "limit" scales from min to max-value
+				right:Size.min(50),         // "min"   scales high but not lower as min-value
+				// right:10                 // or can be a fixed value.. same as .limit(10,10)
 				
-				// for "span" they are reaching its min and max at the same time while scaling
-				// in a row, but can be scaled higher as max
-				top:   Size.span(50, 100),
-				height:Size.span(200, 400),
-				bottom:Size.span(50, 100),
+				// for "span" they are reaching its min and max at the same time
+				// while scaling in a row, but can be scaled higher as max
+				top:   Size.span(50 , 100),
+				height:Size.span(200, 600),
+				bottom:Size.span(50 , 100),
 			},
-			// childs
-			[
-				// Box is shortcut for LayoutContainer(ContainerType.BOX, ...)
-				new Box(red,    {left:0, width:300, height:100, bottom:Size.min(100)} ),
-				new Box(blue,   {right:0, width:300, height:100, bottom:0} ),
-				new Box(yellow, {width:100, height:Size.limit(100, 300)} )
-			]
-		);
+			[	
+				// three child containers inside green box
+				new Box( yellow,
+				{	left:Size.max(10), width:Size.limit(100, 500), right:Size.max(10),
+					height:Size.limit(100, 300)
+				}),
+				new Box( red,
+				{ 	left:0, width:300,
+					height:100, bottom:Size.min(100)
+				}),
+				new Box( blue,
+				{	width:300, right:0,
+					height:100, bottom:0
+				}),
+				
+			]),	
+		]);
 		
-		greenLC.init();
-		greenLC.update(Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
+		layoutContainer.init();
+		layoutContainer.update(Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
 		
-		//greenLC.hide();
-		//greenLC.showt();
 		
-		rootLayoutContainer = greenLC;
-		
+		// window resize is updating outer Box of rootLayoutContainer  
 		Lib.current.stage.addEventListener( Event.RESIZE, function(e) onWindowResize( Lib.current.stage.stageWidth, Lib.current.stage.stageHeight ) );
-		Lib.current.stage.addEventListener( MouseEvent.MOUSE_MOVE, onMouseMove );
+		
+		// to simmulate resizing via moving mouse after pressing left mousebutton
 		Lib.current.stage.addEventListener( MouseEvent.MOUSE_UP,   onMouseUp );
+		Lib.current.stage.addEventListener( MouseEvent.MOUSE_MOVE, onMouseMove );
 	}
 	
-	function onWindowResize (width:Float, height:Float):Void
-	{
-		display.width = width;
-		display.height = height;
-		if (rootLayoutContainer != null) rootLayoutContainer.update(width, height);
-	}
 	
+	
+	// ----------------- RESIZE EVENT ------------------------------
+	
+	function onWindowResize (width:Float, height:Float):Void {
+		if (layoutContainer != null) layoutContainer.update(width, height);
+	}
+		
 	// ----------------- MOUSE EVENTS ------------------------------
+	
 	var sizeEmulation = false;
 	
 	function onMouseMove (e:MouseEvent) {
-		if (sizeEmulation && rootLayoutContainer != null) {
-			display.width = e.stageX;
-			display.height = e.stageY;
-			rootLayoutContainer.update(e.stageX, e.stageY);
-		}
+		if (sizeEmulation && layoutContainer != null)
+			layoutContainer.update(e.stageX, e.stageY);
 	}
 	
 	function onMouseUp (e:MouseEvent) {
 		sizeEmulation = !sizeEmulation; 
-		if (sizeEmulation) {
-			onMouseMove(e);
-		}
-		else {
-			display.width = Lib.current.stage.stageWidth;
-			display.height = Lib.current.stage.stageHeight;
-			if (rootLayoutContainer != null) rootLayoutContainer.update(Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
-		}
+		if (sizeEmulation) onMouseMove(e);
+		else if (layoutContainer != null)
+			layoutContainer.update(Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
 	}
 	
 }
